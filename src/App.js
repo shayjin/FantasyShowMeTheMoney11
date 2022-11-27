3d65f293941d9cbb9dfbee689eabf66aaeaf0e0d
Jay Shin <shin.810@osu.edu>
2022-11-26 21:34:36 -0500

    gang gang gang

---
+++ src/App.js
@@ -0,0 +1,202 @@
+import logo from './logo.svg';
+import './App.css';
+import { Rules } from "./Rules";
+import { Strategy } from "./Strategy";
+import {Link, Route, Routes, BrowserRouter } from 'react-router-dom';
+
+
+function NavBar() {
+  return (
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="/">판타지 쇼미더머니 11</a>
+      
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+          <a class="nav-item nav-link" href="/">현황</a>
+          <a class="nav-item nav-link" href="/Rules">규칙</a>
+          <a class="nav-item nav-link" href="/Strategy" aria-disabled="true">전략</a>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+function ScoreBoard(props) {
+  var p = [[], [], [] ,[]];
+  var points = {};
+
+  for (var j = 0; j < 4; j++) {
+
+    p[j].push(props.players[j+1]);
+    var point = 0;
+
+    for (var i = 0; i < props.possessions[j].length; i++) {
+      var ddc;
+      ddc = props.possessions[j][i];
+      p[j].push(ddc);
+      point += props.rappers[props.possessions[j][i]];
+    }
+
+    points[j] = point;
+  }
+  var items = Object.keys(points).map(
+    (key) => { return [key, points[key]] });
+
+  items.sort(
+    (first, second) => { return first[1] - second[1] }
+  );
+  
+  var keys = items.map(
+    (e) => { return e[0] });
+  
+  
+  var index = 1;
+
+  keys=keys.reverse();
+
+  var pick = [];
+
+  for (var i = 1; i <= 6; i++) {
+    pick.push(
+      <th scope="col">{i}픽</th>
+    )
+  }
+
+  return (
+    <table class="table table-hover table-sm">
+      <thead class="thead thead-dark">
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Player</th>
+          {pick}
+          <th scope="col">점수</th>
+        </tr>
+      </thead>
+      <tbody>
+          {keys.map(x => (
+                <tr>
+                  <th scope="row" key={p[x]}>{index++}</th>
+                    {p[x].map (y => (
+                      <td key={x + y}>{y}</td>
+                    ))}
+                  <td key={index}>{points[x]}</td>
+                </tr>
+            ))}
+      </tbody>
+    </table>
+  );
+}
+
+function Points() {
+  return (
+    <table class="table">
+      <tbody>
+      <tr>
+        <td class="table-danger">1등</td>
+        <td class="table-danger">7점</td>
+      </tr>
+      <tr>
+        <td class="table-primary">2등</td>
+        <td class="table-primary">6점</td>
+      </tr>
+      <tr>
+        <td class="table-success">3등</td>
+        <td class="table-success">5점</td>
+      </tr>
+      <tr>
+        <td class="table-info">4등</td>
+        <td class="table-info">4점</td>
+      </tr>
+      <tr>
+        <td class="table-active">세미파이널 진출 (8명)</td>
+        <td class="table-active">3점</td>
+      </tr>
+      <tr>
+        <td class="table-light">본선 1차 진출 (14명)</td>
+        <td class="table-light">2점</td>
+      </tr>
+      <tr>
+        <td class="table-dark">디스전 진출  (16명)</td>
+        <td class="table-dark">1점</td>
+      </tr>
+      <tr>
+        <td class="table-warning">음원미션 진출  (8명)</td>
+        <td class="table-warning">0점</td>
+      </tr>
+      </tbody>
+    </table>
+  );
+}
+
+function Main() {
+  let players = {1: "Ella", 2: "Jin", 3: "Shay", 4: "Woong"};
+
+  let possessions = [
+    ["", "", "", "", "", ""],
+    ["", "", "", "", "", ""],
+    ["", "", "", "", "", ""],
+    ["", "", "", "", "", ""],
+  ];
+
+  // 탈락자
+  let 음원 = [];
+  let 디스 = [];
+  let 본선 = [];
+  let 세미 = [];
+  let fourth = "";
+  let third = "";
+  let second = "";
+  let first = "";
+
+  let rounds = [음원, 디스, 본선,세미, fourth, third, second, first];
+
+  var curr = 0;
+
+  for (var round in rounds) {
+    if (rounds[round].length != 0 || rounds[round] != "") {
+      curr += 1;
+    }
+  }
+
+  let rappers = {
+    "박명훈": curr, "신세인": curr, "토이고": curr, "제이켠": curr, "잠비노": curr, "이영지": curr,
+    "로스": curr, "맥대디": curr, "던말릭": curr, "언오피셜보이": curr, "칸": curr, "허성현": curr,
+    "다민이": curr, "김도윤": curr, "NSW yoon": curr, "크루셜스타": curr, "QM": curr, "김재욱": curr,
+    "노윤하": curr, "블라세": curr, "칠린호미": curr, "키츠요지": curr, "폴로다레드": curr, "플리키뱅": curr
+  };
+
+  for (var round in rounds) {
+    for (var rapper in rounds[round]) {
+      rapper = rounds[round][rapper];
+      rappers[rapper] = parseInt(round);
+    }
+  }
+
+  return (
+    <div>
+      <ScoreBoard players={players} possessions={possessions} rappers={rappers}></ScoreBoard>
+      <Points></Points>
+    </div>
+    
+  );
+}
+
+function App() {
+  return (
+    <>
+        <NavBar></NavBar>
+        <Routes>
+          <Route path="/Portfolio" element={<Main />} />
+          <Route path="" element={<Main />} />
+          <Route path="/Rules" element={<Rules />} />
+          <Route path="Strategy" element={<Strategy />} />
+          <Route path="*" element={<p>Path not resolved</p>} />
+        </Routes>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Rules.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import Main from './App';
+import './App.css';
+import {Link, Route, Routes, BrowserRouter } from 'react-router-dom';
+
+export const Rules = () => {
+    
+    return (
+        <>
+            <p className="des">
+                안녕하세요. 소개 치우고. 바로 규칙 설명 들어갑니다. <br/><br/>
+                1등 - <b>7</b>점<br/>
+                2등 - <b>6</b>점<br/>
+                3등 - <b>5</b>점<br/>
+                4등 - <b>4</b>점<br/>
+                세미파이널 진출 (8명) - <b>3</b>점<br/>
+                본선 1차 진출 (14명) - <b>2</b>점<br/>
+                디스전 진출  (16명) - <b>1</b>점<br/>
+                음원미션 탈락  (8명) - <b>0</b>점<br/>
+
+                <br/>
+                매주 게임 업데이트가 진행됩니다. 자기 팀에 살아있는 래퍼수 만큼, 현재 라운드 높이에 따라 점수를 얻습니다.<br/>
+                예시: 내팀 (토이고, 이영지, 잠비노, 제이켠, 박명훈, 신세인) | 음원미션에서 박명훈, 신세인 탈락 | 디스전에서 제이켠 탈락 | 지금 현재 날짜는 본선 방영 전 (이라고 가정)<br/>
+                현재 점수는 2(1) + 1(1) + 3(3) = 11 점이 됩니다. 방송이 방영하면 업데이트 됩니다.<br/><br/>
+                4명이니까 각자 플레이어 1, 2, 3, 4 중 하나를 선택합니다. 나이순으로 선택합시다ㅋ. 뽑기 순서는 이겁니다: <br/>
+
+                (Player 1 -&gt; Player 2 -&gt; Player 3 -&gt; Player 4 -&gt; Player 4 -&gt; Player 3 -&gt; Player 2 -&gt; Player 1) 를 <b>3번</b> 진행합니다.<br/>
+                총 24번, 한 사이클에 8번의 뽑기, 각 플레이어마다 6번의 뽑기 차례가 옵니다.<br/><br/>
+                <b>뽑기 순서:</b> <br/>
+                <b>싸이클 1:</b> Player 1 -&gt; Player 2 -&gt; Player 3 -&gt; Player 4<br/>
+                <b>싸이클 2:</b> Player 4 -&gt; Player 3 -&gt; Player 2 -&gt; Player 1<br/>
+                <b>싸이클 3:</b> Player 1 -&gt; Player 2 -&gt; Player 3 -&gt; Player 4<br/>
+                <b>싸이클 4:</b> Player 4 -&gt; Player 3 -&gt; Player 2 -&gt; Player 1<br/>
+                <b>싸이클 5:</b> Player 1 -&gt; Player 2 -&gt; Player 3 -&gt; Player 4<br/>
+                <b>싸이클 6:</b> Player 4 -&gt; Player 3 -&gt; Player 2 -&gt; Player 1<br/>
+            </p>
+        </>
+    );
+}
